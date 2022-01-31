0e00554cb4d5d76ed518d174acd3b9b8da54192b
DoctorRidley <18543868+DoctorRidley@users.noreply.github.com>
2022-01-31 15:01:40 -0800

    Started unit tests for UserRepo class

---
+++ src/test/scala/repos/UserRepoSpec.scala
@@ -0,0 +1,38 @@
+package clients
+
+
+import models.User
+
+
+import org.scalatest.FunSuite
+
+
+class HiveClientSepc extends FunSuite {
+
+    test("Connection to Hive") {
+
+
+    }
+
+    test("") {
+
+
+    }
+
+    test("Reading Existing Account") {
+
+//        val found: User = UserRepo.Read("Admin", "Users")
+//        val target: User = new User("Admin", "root", "Admin")
+
+//        assert(found.toString() == target.toString())
+
+    }
+
+    test("Reading Non-Existent Account") {
+
+    }
+
+
+
+
+}
