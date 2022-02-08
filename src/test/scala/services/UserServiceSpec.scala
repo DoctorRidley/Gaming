5f02e87e728b6bf5149df0ee46e1fa2db32ed177
DoctorRidley <18543868+DoctorRidley@users.noreply.github.com>
2022-02-08 13:47:31 -0800

    Incomplete tests fail

--- src/test/scala/services/UserServiceSpec.scala
+++ src/test/scala/services/UserServiceSpec.scala
@@ -12,23 +12,25 @@ class UserServiceSpec extends FunSuite {
     test("Logging into Existing Account") {
 
         //UserService.Login("Admin", "root")
+        assert(false)
     }
 
     test ("Incorrect Username") {
 
         //UserService.Login("asdifuashfaisfdkj", ";asklfanw.fe")
+        assert(false)
     }
 
     test ("Incorrect Password") {
-
+        assert(false)
     }
 
     test("Update Username") {
-
+        assert(false)
     }
 
     test ("Update Password") {
-
+        assert(false)
     }
 
 }
