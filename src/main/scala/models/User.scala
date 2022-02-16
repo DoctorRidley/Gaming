293e829cf18cfbaa89d178c967b03df0bf89fb69
DoctorRidley <18543868+DoctorRidley@users.noreply.github.com>
2022-02-15 20:41:06 -0800

    Created LocalDateTime field

--- src/main/scala/models/User.scala
+++ src/main/scala/models/User.scala
@@ -2,12 +2,15 @@ package models
 
 import sessions.UserSession
 
+import java.time.LocalDateTime
+
 class User(i: Int, u: String, p: String, t: String) {
 
     private var id: Int = i
     private val username: String = u
     private val password: String = p
     private val user_type: String = t
+    private val temp_date: LocalDateTime = null
 
     def this(u: String, p: String) = this(-1, u, p, "Standard")
 
